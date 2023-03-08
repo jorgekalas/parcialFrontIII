429661b9a46052c43a29f55120085ad4af6aeeb3
jorgekalas <jorgekalas@gmail.com>
2023-03-08 20:28:15 -0300

    pushing front 3 exam resolution

--- parcial-frontIII-kalas-jorge/src/App.jsx
+++ parcial-frontIII-kalas-jorge/src/App.jsx
@@ -1,21 +1,81 @@
 import './App.css'
+import { useState } from 'react'
 import Card from './components/Card'
 
 function App() {
+
+  //1. useState
+
+  const [nombre, setNombre] = useState("");
+  const [signo, setSigno] = useState("");
+  const [anioNacimiento, setAnioNacimiento] = useState("");
+  const [validationError, setValidationError] = useState("");
+
+  //2. Handlers
+
+  const onChangeNombre = (event) => {setNombre(event.target.value.trim())}
+  const onChangeSigno = (event) => {setSigno(event.target.value.trim())}
+  const onChangeAnioNacimiento = (event) => {setAnioNacimiento(event.target.value)}
+
+  //3. Validaciones
+
+  const validarNombre = (nombre) => {
+    //Nota: el nombre ya ingresa a la validación seteado con el trim, por lo que no es necesario volver a utilizar este método
+    return nombre.length >= 3;
+  }
+
+  const validarSigno = (signo) => {
+    //Nota: a pesar de no solicitarlo la consigna, prefiero mantener el trim también en este input, por una cuestión de homogeneidad en los criterios de validación
+    return signo.length >= 6;
+  }
+
+  const validarAnioNacimiento = (anioNacimiento) => {
+    const fechaActual = new Date();
+    const anioActual = fechaActual.getFullYear();
+    return anioNacimiento > 1900 && anioNacimiento <= anioActual;
+  }
+
+//4. Handler para el formulario (evento submit)
+
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+
+    const esNombreValido = validarNombre(nombre);
+    const esSignoValido = validarSigno(signo);
+    const esAnioNacimientoValido = validarAnioNacimiento(anioNacimiento);
+
+    if(esNombreValido && esSignoValido && esAnioNacimientoValido){
+      setValidationError(false);
+    } else{
+      setValidationError("Por favor, chequea que la información sea correcta");
+    }
+
+  }
+
+//5. Return
+
   return (
     <div className="App">
 
-      <h1>Carga de estudiantes</h1>
+      <h1>Tu número de la suerte 🍀</h1>
 
-      <form>
-        <label>label</label>
-        <input type="text" />
+      <form onSubmit = {onSubmitForm}>
+
+        <input type="text" value={nombre} onChange={onChangeNombre} placeholder="Ingresa tu nombre" disabled={validationError===false}/>
+        <input type="text" value={signo} onChange={onChangeSigno} placeholder="Ingresa tu signo" disabled={validationError===false}/>
+        <input type="number" value={anioNacimiento} onChange={onChangeAnioNacimiento} placeholder="Ingresa tu año de nacimiento" disabled={validationError===false}/>
+        
+        <button type="submit" disabled={validationError===false}>Enviar</button>
+
+        <div className="divError">{validationError}</div>
+
+        {validationError===false && <Card nombre = {nombre} signo = {signo} anioNacimiento = {anioNacimiento} />}
+
       </form>
 
-      <Card />
-
     </div>
-  );
+  )
 }
 
-export default App
+export default App;
+
--- parcial-frontIII-kalas-jorge/src/components/Card.jsx
+++ parcial-frontIII-kalas-jorge/src/components/Card.jsx
@@ -1,9 +1,26 @@
-const Card = () => {
-    return(
-        <div>
-            Hola, soy una Card
-        </div>
-    );
+import styles from './Card.module.css'
+
+const Card = ({nombre, signo, anioNacimiento}) => {
+
+    //1. Variable para calcular el número de la suerte
+    const numeroDeLaSuerte = Math.round((anioNacimiento * Math.random())/25);
+
+    //2. Handler para recargar el sitio
+    const onClickToReload = () =>{ location.reload(); }
+
+    //3. Return
+    return (
+        <>
+            <div className = {styles.divNumeroSuerte}>
+                <p className ={styles.nombre}>¡Hola {nombre}!</p>
+                <p className = {styles.textoSup}>Según tu signo ({signo}) y tu año de nacimiento ({anioNacimiento}),</p>
+                <p className = {styles.textoInf}>puedo decirte que tu número de la suerte para hoy, es el:</p>
+                <p className = {styles.numero}>✨ {numeroDeLaSuerte} ✨</p>
+            </div>
+            <marquee>🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀</marquee>
+            <button onClick={onClickToReload}>Volver a empezar</button>
+        </>
+    )
 }
 
 export default Card;
